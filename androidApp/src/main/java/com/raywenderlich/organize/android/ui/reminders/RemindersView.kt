77a573768084ded08653a6e1f17d204171bc7261
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2023-04-26 23:54:33 +0100

    refactor: Update Android RemindersView to read the VM

--- androidApp/src/main/java/com/raywenderlich/organize/android/ui/reminders/RemindersView.kt
+++ androidApp/src/main/java/com/raywenderlich/organize/android/ui/reminders/RemindersView.kt
@@ -34,27 +34,62 @@
 
 package com.raywenderlich.organize.android.ui.reminders
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.neverEqualPolicy
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.organize.domain.Reminder
+import com.raywenderlich.organize.presentation.RemindersViewModel
 
 @Composable
 fun RemindersView(
+  viewModel: RemindersViewModel = RemindersViewModel(),
   onAboutButtonClick: () -> Unit,
 ) {
   Column {
     Toolbar(onAboutButtonClick = onAboutButtonClick)
-    ContentView()
+    ContentView(viewModel = viewModel)
   }
 }
 
@@ -62,32 +97,137 @@ fun RemindersView(
 private fun Toolbar(
   onAboutButtonClick: () -> Unit,
 ) {
-  TopAppBar(
-    title = { Text(text = "Reminders") },
-    actions = {
-      IconButton(onClick = onAboutButtonClick) {
-        Icon(
-          imageVector = Icons.Outlined.Info,
-          contentDescription = "About Device Button",
-        )
-      }
+  TopAppBar(title = { Text(text = "Reminders") }, actions = {
+    IconButton(onClick = onAboutButtonClick) {
+      Icon(
+        imageVector = Icons.Outlined.Info,
+        contentDescription = "About Device Button",
+      )
     }
-  )
+  })
 }
 
 @Composable
-private fun ContentView() {
-  Box(
-    modifier = Modifier.fillMaxSize(),
-    contentAlignment = Alignment.Center
-  ) {
-    Text("Hello World!")
+private fun ContentView(viewModel: RemindersViewModel) {
+  var textFieldValue by remember { mutableStateOf("") }
+  var reminders by remember {
+    mutableStateOf(listOf<Reminder>(), policy = neverEqualPolicy())
   }
+  val focusManager = LocalFocusManager.current
+  val focusRequester = FocusRequester.Default
+  var shouldFocusOnTextField by remember { mutableStateOf(false) }
+
+  viewModel.onRemindersUpdated = {
+    reminders = it
+  }
+
+  LazyColumn(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(top = 4.dp, bottom = 8.dp)
+  ) {
+    items(items = reminders) { item ->
+      val onItemClick = {
+        focusManager.clearFocus()
+        viewModel.markReminder(id = item.id, isCompleted = !item.isCompleted)
+      }
+
+      ReminderItem(
+        title = item.title,
+        isCompleted = item.isCompleted,
+        modifier = Modifier
+          .fillMaxWidth()
+          .clickable(enabled = true, onClick = onItemClick)
+          .padding(horizontal = 16.dp, vertical = 4.dp)
+      )
+    }
+
+    item {
+      val onSubmit = {
+        viewModel.createReminder(title = textFieldValue)
+        textFieldValue = ""
+        shouldFocusOnTextField = true
+      }
+
+      NewReminderTextField(
+        value = textFieldValue,
+        onValueChange = { textFieldValue = it },
+        onSubmit = onSubmit,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(vertical = 8.dp, horizontal = 16.dp)
+          .focusRequester(focusRequester)
+      )
+
+      LaunchedEffect(reminders) {
+        if (shouldFocusOnTextField) {
+          focusRequester.requestFocus()
+          shouldFocusOnTextField = false
+        }
+      }
+    }
+  }
+}
+
+@Composable
+private fun ReminderItem(
+  title: String,
+  isCompleted: Boolean,
+  modifier: Modifier = Modifier,
+) {
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.Start,
+    modifier = modifier
+  ) {
+    RadioButton(
+      selected = isCompleted, onClick = null
+    )
+
+    Text(
+      text = title,
+      style = if (isCompleted) {
+        MaterialTheme.typography.body1.copy(
+          textDecoration = TextDecoration.LineThrough,
+          fontStyle = FontStyle.Italic,
+          color = Color.Gray,
+        )
+      } else {
+        MaterialTheme.typography.body1
+      },
+      modifier = Modifier.padding(8.dp),
+    )
+  }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun NewReminderTextField(
+  value: String,
+  onValueChange: (String) -> Unit,
+  onSubmit: () -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  OutlinedTextField(value = value,
+    onValueChange = onValueChange,
+    placeholder = { Text("Add a new reminder here") },
+    keyboardOptions = KeyboardOptions.Default.copy(
+      capitalization = KeyboardCapitalization.Words,
+      keyboardType = KeyboardType.Text,
+      imeAction = ImeAction.Done,
+    ),
+    keyboardActions = KeyboardActions(onDone = { onSubmit() }),
+    modifier = modifier.onPreviewKeyEvent { event: KeyEvent ->
+        if (event.key == Key.Enter) {
+          onSubmit()
+          return@onPreviewKeyEvent true
+        }
+        false
+      })
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun RemindersViewPreview() {
-  RemindersView {
-  }
+  RemindersView {}
 }
