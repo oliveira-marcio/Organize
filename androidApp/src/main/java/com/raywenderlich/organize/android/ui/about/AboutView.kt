0d3bc2659356b53e68ab463fe8b61ff90f2dd560
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2023-04-26 17:14:09 +0100

    feat: Complete Android app AboutView

--- androidApp/src/main/java/com/raywenderlich/organize/android/ui/about/AboutView.kt
+++ androidApp/src/main/java/com/raywenderlich/organize/android/ui/about/AboutView.kt
@@ -35,14 +35,27 @@
 package com.raywenderlich.organize.android.ui.about
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.organize.Platform
+import kotlin.math.max
+import kotlin.math.min
 
 @Composable
 fun AboutView(
@@ -73,6 +86,58 @@ private fun Toolbar(
 
 @Composable
 private fun ContentView() {
+  val items = makeItems()
+
+  LazyColumn(
+    modifier = Modifier.fillMaxSize(),
+  ) {
+    items(items) { row ->
+      RowView(title = row.first, subtitle = row.second)
+    }
+  }
+}
+
+private fun makeItems(): List<Pair<String, String>> {
+  //1
+  val platform = Platform()
+
+  //2
+  val items = mutableListOf(
+    Pair("Operating System", "${platform.osName} ${platform.osVersion}"),
+    Pair("Device", platform.deviceModel),
+    Pair("CPU", platform.cpuType)
+  )
+
+  //3
+  platform.screen?.let {
+    val max = max(it.width, it.height)
+    val min = min(it.width, it.height)
+
+    items.add(Pair("Display", "${max}×${min} @${it.density}x"))
+  }
+
+  return items
+}
+
+@Composable
+private fun RowView(
+  title: String,
+  subtitle: String,
+) {
+  Column(modifier = Modifier.fillMaxWidth()) {
+    Column(Modifier.padding(8.dp)) {
+      Text(
+        text = title,
+        style = MaterialTheme.typography.caption,
+        color = Color.Gray,
+      )
+      Text(
+        text = subtitle,
+        style = MaterialTheme.typography.body1,
+      )
+    }
+    Divider()
+  }
 }
 
 @Preview(showBackground = true)
