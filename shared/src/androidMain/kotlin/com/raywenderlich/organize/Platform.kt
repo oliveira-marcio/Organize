2a042a06c3c142af5f002002cf05452d9aeaffae
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2023-04-26 16:39:36 +0100

    feat: Add platform-specific implementations

--- shared/src/androidMain/kotlin/com/raywenderlich/organize/Platform.kt
+++ shared/src/androidMain/kotlin/com/raywenderlich/organize/Platform.kt
@@ -34,6 +34,27 @@
 
 package com.raywenderlich.organize
 
+import android.content.res.Resources
+import android.os.Build
+import android.util.Log
+import kotlin.math.round
+
 actual class Platform actual constructor() {
-    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+  actual val osName = "Android"
+  actual val osVersion = "${Build.VERSION.SDK_INT}"
+  actual val deviceModel = "${Build.MANUFACTURER} ${Build.MODEL}"
+  actual val cpuType = Build.SUPPORTED_ABIS.firstOrNull() ?: "---"
+  actual val screen: ScreenInfo? = ScreenInfo()
+
+  actual fun logSystemInfo() {
+    Log.d("Platform", deviceInfo)
+  }
+}
+
+// 8
+actual class ScreenInfo actual constructor() {
+  private val metrics = Resources.getSystem().displayMetrics
+  actual val width = metrics.widthPixels
+  actual val height = metrics.heightPixels
+  actual val density = round(metrics.density).toInt()
 }
